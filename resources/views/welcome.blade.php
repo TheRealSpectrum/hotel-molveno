d1fdd1ba4dba4ce8292cfe66a7c939b22e3a389c
matt123455 <mwildhagen@hotmail.com>
2021-08-24 15:13:11 +0200

    updae

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,173 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Molveno resort</title>
+
+  <link rel="icon" href="/favicon.ico">
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+  <header>
+      <div>
+      <img src="/images/logo.jpg">
+        <ul>
+            <li>Kamers</li>
+            <li>Faciliteiten</li>
+            <li>Galerij</li>
+            <li>Restaurant </li>
+            <li>Locatie</li>
+            <li>Contact</li>
+        </ul>
+        <button>Log in</button>
+        <button>Registreer </button>
+    </div>
+  </header>
+
+  <main>
+      <div class="main1">
+          <svg></svg>
+          <div>
+              <div>
+                <h1>Molveno resort</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna. Proin libero nunc consequat interdum. Lectus quam id leo in vitae turpis massa. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Interdum varius sit amet mattis. In eu mi bibendum neque egestas. Tincidunt eget nullam non nisi est sit. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Fames ac turpis egestas sed tempus. Volutpat lacus laoreet non curabitur gravida. Orci sagittis eu </p>
+              </div>
+              <div>
+                <h1>Omgeving</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna. Proin libero nunc consequat interdum. Lectus quam id leo in vitae turpis massa. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Interdum varius sit amet mattis. In eu mi bibendum neque egestas. Tincidunt eget nullam non nisi est sit. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Fames ac turpis egestas sed tempus. Volutpat lacus laoreet non curabitur gravida. Orci sagittis eu </p>
+              </div>
+              <div>
+                <h1>Activiteiten</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna. Proin libero nunc consequat interdum. Lectus quam id leo in vitae turpis massa. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Interdum varius sit amet mattis. In eu mi bibendum neque egestas. Tincidunt eget nullam non nisi est sit. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Fames ac turpis egestas sed tempus. Volutpat lacus laoreet non curabitur gravida. Orci sagittis eu </p>
+              </div>
+          </div>
+      </div>
+      <div class="main2">
+          <div>
+              <h1>Onze kamers</h1>
+              <h3>Kamer optie 1</h3>
+              <svg></svg>
+          </div>
+          <div>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+          </div>
+      </div>
+      <div class="main3">
+        <svg></svg>
+        <h1>Hotel faciliteiten</h1>
+        <div>
+            <div>
+                <ul>
+                    <li>Wifi</li>
+                    <li>Parkeren bij het hotel</li>
+                    <li>Airconditioning</li>
+                    <li>Zwembad</li>
+                    <li>Sauna(VIP)</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Wifi</li>
+                    <li>Parkeren bij het hotel</li>
+                    <li>Airconditioning</li>
+                    <li>Zwembad</li>
+                    <li>Sauna(VIP)</li>
+                </ul>
+            </div>
+        </div>
+      </div>
+      <div class="main4">
+          <h1>De omgeving</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+          <div>
+              <svg></svg>
+              <h2>Locatie</h2>
+              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
+          </div>
+          <div>
+              <svg></svg>
+              <h2>Natuur</h2>
+              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
+          </div>
+          <div>
+              <svg></svg>
+              <h2>Ontspanning</h2>
+              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
+          </div>
+      </div>
+      <div class="main5">
+          <div>
+              <h1>Restaurant</h1>
+              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+              <div>
+                  <svg></svg>
+                  <h1>0039 – 934 4444 32</h1>
+              </div>
+          </div>
+          <div>
+              <svg></svg>
+          </div>
+      </div>
+      <div class="main6">
+          <svg></svg>
+          <div>
+            <svg></svg>
+            <svg></svg>
+            <svg></svg>
+          </div>
+      </div>
+      <div class="main7">
+          <h1>Molveno resort</h1>
+          <button>Boek een kamer</button>
+          <button>Neem contact op</button> 
+      </div>
+  </main>
+
+  <footer>
+      <div>
+        <div>
+            <h3>Marketing/werk</h3>
+            <ul>
+                <li>Adverteren</li>
+                <li>Vacatures</li>
+            </ul>
+        </div>
+        <div>
+            <h3>Privacy</h3>
+            <ul>
+                <li>Cookies</li>
+                <li>Algemene voorwaarden</li>
+            </ul>
+        </div>
+        <div>
+            <h3>Molveno resort</h3>
+            <ul>
+                <li>Contact</li>
+                <li>Over ons</li>
+                <li>Locatie</li>
+            </ul>
+        </div>
+      </div>
+      <div>
+          <div>
+              <p>Volg ons:</p>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+              <svg></svg>
+          </div>
+      </div>
+  </footer>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
