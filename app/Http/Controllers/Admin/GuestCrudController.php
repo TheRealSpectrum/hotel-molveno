722ddcdf5eb7fdfd76af7553127cf8b8b0667a6e
matt123455 <mwildhagen@hotmail.com>
2021-09-17 12:26:41 +0200

    updated guest validation

--- app/Http/Controllers/Admin/GuestCrudController.php
+++ app/Http/Controllers/Admin/GuestCrudController.php
@@ -66,7 +66,7 @@ class GuestCrudController extends CrudController
         CRUD::field("last_name");
         CRUD::field("email");
         CRUD::field("address")->type("address_algolia");
-        CRUD::field("phone")->type("number");
+        CRUD::field("phone");
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
