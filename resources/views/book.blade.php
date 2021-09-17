f0be4e6c07fa6d6e7e9117145703c225777ab797
daanminderman <81742169+daanminderman@users.noreply.github.com>
2021-09-17 11:48:46 +0200

    added action, post and number type

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <h1 class="text-2xl font-medium flex justify-center pt-4">Please fill in the form to make a reservation:</h1> 
 <div class="flex justify-center p-4 border-b-2"> 
-    <form>
+    <form action="roomtype" method="post">
         <label class="font-medium">Check-in:</label><br>
         <input class="bg-gray-100 w-full border-2 rounded" type="date"><br>
         <label class="font-medium">Check-out:<label><br>
         <input class="bg-gray-100 w-full border-2 rounded" type="date"><br>
         <label class="font-medium">How many adults?</label><br>
-        <input class="bg-gray-100 w-full border-2 rounded" type="field"><br>
+        <input class="bg-gray-100 w-full border-2 rounded" type="number"><br>
         <label class="font-medium">How many children?</label><br>
-        <input class="bg-gray-100 w-full border-2 rounded" type="field"><br>
+        <input class="bg-gray-100 w-full border-2 rounded" type="number"><br>
         <label class="font-medium">How many rooms?</label><br>
-        <input class="bg-gray-100 w-full border-2 rounded" type="field"><br>
-        <input type="submit" value="Make reservation" class="inline-block text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 w-full lg:mt-0"> 
+        <input class="bg-gray-100 w-full border-2 rounded" type="number" name="rooms"><br>
+        <input type="submit" value="Select room(s)" href="/selectroom" class="inline-block text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 w-full lg:mt-0"> 
     </form>
 </div>
 @endsection
---
+++ resources/views/roomtype.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Choose your room type - Molveno Resort')
+@section('content')
+
+        You have selected <?php $_POST["rooms"]; ?> rooms.
+        Please choose the type of rooms you want to stay in:
+        <form>
+
+        </form>
+
+@endsection
