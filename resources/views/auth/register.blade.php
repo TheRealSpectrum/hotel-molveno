a82e6efe2e2a183739de88180a7452a5caa24fb8
Ian <62475005+CyberAzuuL@users.noreply.github.com>
2021-09-23 12:27:10 +0200

    Added password visibility toggles in guest login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,22 +27,65 @@
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
+            <style>
+                #pw-container {
+                position: relative;
+                }
+
+                #pw-container2 {
+                position: relative;
+                }
+
+                #pw-eye {
+                    position: absolute;
+                    cursor: pointer;
+                    right: 18px;
+                    top: 38px;
+                }
+
+                #pw-eye2 {
+                    position: absolute;
+                    cursor: pointer;
+                    right: 18px;
+                    top: 38px;
+                }
+
+                #pw-eye-slash {
+                    position: absolute;
+                    cursor: pointer;
+                    display: none;
+                    right: 18px;
+                    top: 38px;
+                }
+
+                #pw-eye-slash2 {
+                    position: absolute;
+                    cursor: pointer;
+                    display: none;
+                    right: 18px;
+                    top: 38px;
+                }
+            </style>
+            <div id="pw-container" class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
                                 required autocomplete="new-password" />
+                <i id="pw-eye" onclick="passvisToggle()" class="las la-eye"></i>
+                <i id="pw-eye-slash" onclick="passvisToggle()" class="las la-eye-slash"></i>
             </div>
 
             <!-- Confirm Password -->
-            <div class="mt-4">
+            <div id="pw-container2" class="mt-4">
                 <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required />
+                <i id="pw-eye2" onclick="confPassvisToggle()" class="las la-eye"></i>
+                <i id="pw-eye-slash2" onclick="confPassvisToggle()" class="las la-eye-slash"></i>
             </div>
 
             <div class="flex items-center justify-end mt-4">
@@ -55,5 +98,34 @@
                 </x-button>
             </div>
         </form>
+        <script>
+            function passvisToggle() {
+            let x = document.getElementById("password");
+
+            if (x.type === "password") {
+                document.getElementById("pw-eye").style.display = "none";
+                document.getElementById("pw-eye-slash").style.display = "block";
+                x.type = "text";
+            } else {
+                document.getElementById("pw-eye").style.display = "block";
+                document.getElementById("pw-eye-slash").style.display = "none";
+                x.type = "password";
+            }
+        }
+
+        function confPassvisToggle() {
+            let x = document.getElementById("password_confirmation");
+
+            if (x.type === "password") {
+                document.getElementById("pw-eye2").style.display = "none";
+                document.getElementById("pw-eye-slash2").style.display = "block";
+                x.type = "text";
+            } else {
+                document.getElementById("pw-eye2").style.display = "block";
+                document.getElementById("pw-eye-slash2").style.display = "none";
+                x.type = "password";
+            }
+        }
+        </script>
     </x-auth-card>
 </x-guest-layout>
