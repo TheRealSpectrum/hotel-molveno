179d6fd1a6c806dc903bdbba7d9bb60d23c71001
daanminderman <81742169+daanminderman@users.noreply.github.com>
2021-09-23 15:38:19 +0200

    copied styling to new blade files, deleted obsolete blade files

--- resources/views/booking/step3.blade.php
+++ resources/views/booking/step3.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
-@section('title', 'Choose your room type - Molveno Resort')
+@section('title', 'Finish reservation - Molveno Resort')
 @section('content')
- {{ $data["check_in"] }}
+    You have selected the following:
+    <form action="" method="get">
+        @csrf
+        <p>Room type(s):</p>
+        <p>Room price(s):&euro;</p>
+        <p>Check-in date:</p>
+        <p>Check-out date:</p>
+        <p>Number of adults:</p>
+        <p>Number of children:</p>
+        <input type="submit" value="Finish Reservation" onclick="return confirm('Are you sure?')">
+    </form>
+
+    If you want to switch between room types, please click here:
+    <button href="/roomtype" value="Change room type">
+
+    If your reservation is incorrect, please return to the reservation form here:
+    <button href="/book" value="Return to booking">
 @endsection
