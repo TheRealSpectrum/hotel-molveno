ea2da5c6b70a8b4906cbfc4676c7a5c8501508d8
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-09-24 12:12:36 +0200

    Edit error messages

--- resources/views/booking/step1.blade.php
+++ resources/views/booking/step1.blade.php
@@ -4,9 +4,6 @@
 <h1 class="text-2xl font-medium flex justify-center pt-4">Please fill in the form to make a reservation:</h1> 
 @if (\Session::has("error"))
     <div class="bg-red-200 border-red-600 text-red-600 border-l-4 p-4" role="alert">
-        <p class="font-bold">
-            Error!
-            </p>
             <p>
                 {{ Session::get("error") }}
             </p>
--- resources/views/booking/step2.blade.php
+++ resources/views/booking/step2.blade.php
@@ -5,9 +5,6 @@
 <main>
 	@if (\Session::has("error"))
     <div class="bg-red-200 border-red-600 text-red-600 border-l-4 p-4" role="alert">
-        <p class="font-bold">
-            Error!
-            </p>
             <p>
                 {{ Session::get("error") }}
             </p>
