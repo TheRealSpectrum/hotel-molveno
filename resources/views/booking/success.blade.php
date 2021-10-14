2f2ca3dea46ba519cddfe921635e3318148dabc4
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-10-14 19:36:37 +0200

    Update styling on reservation success view

--- resources/views/booking/success.blade.php
+++ resources/views/booking/success.blade.php
@@ -3,14 +3,20 @@
 @section('content')
 
 <div class="flex-grow">
-    <div class="grid grid-cols-2">
+    <div class="grid grid-rows-6">
         <div class="flex justify-center mt-5">
             <h1 class="text-5xl font-bold">
-                Thank You.
+               Thank You.
             </h1>   
         </div>
         <div class="flex justify-center">
             <p class="text-xl">Your reservation was completed succesfully</p>
+        </div>
+        <div class="flex justify-center row-span-2">
+            <img class="w-40" src="{{ asset("images/Logo Molveno Resort Black.svg") }}" alt="">
+        </div>
+        <div class="flex justify-center">
+            <p class="text-lg italic">An email receipt including the details about your reservation has been sent to the email provided.</p>
         </div>
     </div>
 </div>
