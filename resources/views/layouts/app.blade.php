2841018540e5c4549449113af02f0bf0c8e9251d
matt123455 <mwildhagen@hotmail.com>
2021-10-16 08:33:59 +0200

    wip layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,35 +21,35 @@
 </head>
 
 <body class="flex flex-col min-h-screen">
-    <header class="sticky top-0 z-50">
-        <nav class="flex items-center justify-between flex-wrap bg-gray-100 pr-6 pl-6">
-            <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <header class="md:sticky top-0 z-50">
+        <nav class="flex flex-col justify-between bg-gray-100 px-6 lg:flex-row">
+            <div class="flex items-center flex-shrink-0 lg:mr-12">
                 <a href="{{ url("home") }}">
-                    <img src="{{ asset("images/Logo Molveno Resort Black.svg") }}" alt="Molveno Resort Logo" class="fill-current h-20 w-20 mr-12 ml-4">
+                    <img src="{{ asset("images/Logo Molveno Resort Black.svg") }}" alt="Molveno Resort Logo" class="fill-current h-20 w-20">
                 </a>
             </div>
-            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div class="text-sm lg:flex-grow">
-                    <a href="{{ url('home/#rooms') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+            <div class="lg:flex lg:flex-row lg:items-center lg:justify-between lg:w-full">
+                <div class="flex flex-col lg:flex-row">
+                    <a href="{{ url('home/#rooms') }}" class="text-blue-500 my-2 lg:mx-2">
                         Rooms
                     </a>
-                    <a href="{{ url('home/#facilities') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+                    <a href="{{ url('home/#facilities') }}" class="text-blue-500 my-2 lg:mx-2">
                         Facilities
                     </a>
-                    <a href="{{ url('home/#gallery') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+                    <a href="{{ url('home/#gallery') }}" class="text-blue-500 my-2 lg:mx-2">
                         Gallery
                     </a>
-                    <a href="{{ url('home/#restaurant') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+                    <a href="{{ url('home/#restaurant') }}" class="text-blue-500 my-2 lg:mx-2">
                         Restaurant
                     </a>
-                    <a href="{{ url('home/#location') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+                    <a href="{{ url('home/#location') }}" class="text-blue-500 my-2 lg:mx-2">
                         Location
                     </a>
-                    <a href="{{ url('home/#contact') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600">
+                    <a href="{{ url('home/#contact') }}" class="text-blue-500 my-2 lg:mx-2">
                         Contact
                     </a>
                 </div>
-                <div>
+                <div class="flex flex-col py-6 lg:flex-row lg:items-center lg:py-0">
                     <!-- <a href="{{ route("booking.index") }}" class="inline-block text-2xl px-10 py-4 bg-blue-500 leading-none border rounded-lg font-bold text-white border-blue hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 mr-8 lg:mt-0">Book now</a>
                     @if (auth()->user())
                     <a href="{{ route("account.index") }}" class="inline-block text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 lg:mt-0">Account</a>
@@ -58,13 +58,13 @@
                     <a href="{{ route("login") }}" class="inline-block text-sm px-8 py-3 leading-none border rounded-lg text-blue border-blue-500 hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 lg:mt-0">Sign in</a>
                     <a href="{{ route("register") }}" class="inline-block text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 lg:mt-0">Register</a>
                     @endif -->
-                    <button href="{{ route("booking.index") }}" class="inline-block text-2xl px-10 py-4 bg-blue-500 leading-none border rounded-lg font-bold text-white hover:border-transparent hover:text-teal-500 hover:bg-gray-400">Book now</button>
+                    <a href="{{ route("booking.index") }}" class="inline-block w-48 text-2xl px-8 py-4 bg-blue-500 leading-none border rounded-lg font-bold text-white my-4 lg:mr-12">Book now</a>
                     @if (auth()->user())
-                    <button href="{{ route("account.index") }}" class="inline-block text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 lg:mt-0">Account</button>
-                    <button href="{{ route("logout") }}" class="inline-block text-sm px-8 py-3 leading-none border rounded-lg text-blue border-blue-500 hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 lg:mt-0">Sign out</button>
+                    <a href="{{ route("account.index") }}" class="inline-block w-32 text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue my-4 lg:h-10 lg:mr-2">Account</a>
+                    <a href="{{ route("logout") }}" class="inline-block w-32 text-sm px-8 py-3 leading-none border rounded-lg text-blue border-blue-500 my-4 lg:h-10">Sign out</a>
                     @else
-                    <button href="{{ route("login") }}" class="inline-block text-sm px-8 py-3 leading-none border rounded-lg text-blue border-blue-500 hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 lg:mt-0">Sign in</button>
-                    <button href="{{ route("register") }}" class="inline-block text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue hover:border-transparent hover:text-teal-500 hover:bg-gray-400 mt-4 lg:mt-0">Register</button>
+                    <a href="{{ route("login") }}" class="inline-block w-32 text-sm px-8 py-3 leading-none border rounded-lg text-blue border-blue-500 my-4 lg:h-10 lg:mr-2">Sign in</a>
+                    <a href="{{ route("register") }}" class="inline-block w-32 text-sm px-8 py-3 bg-blue-500 leading-none border rounded-lg text-white border-blue my-4 lg:h-10">Register</a>
                     @endif
                 </div>
             </div>
@@ -74,64 +74,57 @@
     @yield('content')
 
     <footer>
-      <div class="border-b-2 border-t-2 grid grid-cols-2 p-4 px-12 h-72 mt-20">
-            <div class="flex justify-center w-3/4">
+        <div class="flex flex-col items-center border-b-2 border-t-2 lg:grid lg:grid-cols-2 p-4 px-12 mt-20">
+            <div class="flex justify-center w-full">
                 <div id="map" class="h-full w-full rounded-md"></div>
             </div>
-          <div class="flex flex-row justify-end items-center px-12">
-            <div class="h-1/3 ml-12">
-                <h3 class="text-sm font-bold pb-4">Marketing/work</h3>
-                <ul>
-                    <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
-                        Advertising
-                    </a></li>
-                    <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
-                        Vacancies
-                    </a></li>
-                </ul>
+            <div class="flex flex-col lg:flex-row lg:justify-end lg:items-center px-12 lg:h-96">
+                <div class="py-8 lg:h-2/3 lg:ml-12">
+                    <h3 class="text-sm font-bold pb-4">Marketing/work</h3>
+                    <ul>
+                        <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">Advertising</a></li>
+                        <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">Vacancies</a></li>
+                    </ul>
+                </div>
+                <div class="py-8 lg:h-2/3 lg:ml-12">
+                    <h3 class="text-sm font-bold pb-4">Privacy</h3>
+                    <ul>
+                        <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">Cookies</a></li>
+                        <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">Terms and conditions</a></li>
+                    </ul>
+                </div>
+                <div class="py-8 lg:h-2/3 lg:ml-12">
+                    <h3 class="text-sm font-bold pb-4">Molveno resort</h3>
+                    <ul>
+                        <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+                            Contact
+                        </a></li>
+                        <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+                            About us
+                        </a></li>
+                        <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
+                            Location
+                        </a></li>
+                    </ul>
+                </div>
             </div>
-            <div class="h-1/3 ml-12">
-                <h3 class="text-sm font-bold pb-4">Privacy</h3>
-                <ul>
-                    <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
-                        Cookies
-                    </a></li>
-                    <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
-                        Terms and conditions
-                    </a></li>
-                </ul>
+        </div>
+        <div class="flex flex-row justify-between px-12 py-4">
+            <div class="flex flex-col justify-center">
+                <p>© Molveno Resort,  2021</p>
             </div>
-            <div class="h-1/3 ml-12">
-                <h3 class="text-sm font-bold pb-4">Molveno resort</h3>
-                <ul>
-                    <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
-                        Contact
-                    </a></li>
-                    <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
-                        About us
-                    </a></li>
-                    <li><a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-blue-500 hover:text-gray-600 mr-4">
-                        Location
-                    </a></li>
-                </ul>
+            <div class="flex flex-col items-center">
+                <p class="pr-4">Follow us:</p>
+                <div class="flex flex-row">
+                    <a href="http://www.facebook.com" target="_blank"><i class="lab la-facebook text-5xl"></i></a>
+                    <a href="http://www.twitter.com" target="_blank"><i class="lab la-twitter text-5xl"></i></a>
+                    <a href="http://www.instagram.com" target="_blank"><i class="lab la-instagram text-5xl"></i></a>
+                </div>
             </div>
-          </div>
-      </div>
-      <div class="flex flex-row justify-between px-12 py-4">
-          <div class="flex flex-col justify-center">
-            <p>© Molveno Resort,  2021</p>
-          </div>
-          <div class="flex flex-col items-center">
-              <p class="pr-4">Follow us:</p>
-              <div class="flex flex-row">
-                <a href="http://www.facebook.com" target="_blank"><i class="lab la-facebook text-5xl"></i></a>
-                <a href="http://www.twitter.com" target="_blank"><i class="lab la-twitter text-5xl"></i></a>
-                <a href="http://www.instagram.com" target="_blank"><i class="lab la-instagram text-5xl"></i></a>
-              </div>
-          </div>
-      </div>
-      <button onclick="topFunction()" id="myBtn" title="Go to top"><i id="scroll-top-icon" class="las la-chevron-circle-up"></i></button>
+        </div>
+        <button onclick="topFunction()" id="myBtn" title="Go to top"><i id="scroll-top-icon" class="las la-chevron-circle-up"></i></button>
     </footer>
+
     <style>
         #myBtn {
             background-color: black;
@@ -180,22 +173,21 @@
         }
     </script>
     <script>
-// Initialize and add the map
-function initMap() {
-  // The location of Molveno Resort
-  const molvenoResort = { lat: 46.133471, lng: 10.9687150 };
-  // The map, centered at Molveno Resort
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: molvenoResort,
-  });
-  // The marker, positioned at Molveno Resort
-  const marker = new google.maps.Marker({
-    position: molvenoResort,
-    map: map,
-  });
-}
+        // Initialize and add the map
+        function initMap() {
+            // The location of Molveno Resort
+            const molvenoResort = { lat: 46.133471, lng: 10.9687150 };
+            // The map, centered at Molveno Resort
+            const map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 15,
+                center: molvenoResort,
+            });
+            // The marker, positioned at Molveno Resort
+            const marker = new google.maps.Marker({
+                position: molvenoResort,
+                map: map,
+            });
+        }
     </script>
 </body>
 </html>
-
